db615e0c25259e3ea03a9b5758026008e546282a
damien <damien.savoldelli@gmail.com>
2019-11-27 21:05:54 +0100

    Snapshot testing : Update storybook config, link macro context

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
 import { configure } from '@storybook/react';
+import '../src/index.css';
+import requireContext from 'require-context.macro';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+
+const req = requireContext('../src/components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
