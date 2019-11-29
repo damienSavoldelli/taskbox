d1e1bea12b5ad4f720ebde35c82d3c8db5c37ba9
damien <damien.savoldelli@gmail.com>
2019-11-29 12:44:23 +0100

    Refacto : Move test files & snaptshot to test folder

--- src/__TEST__/TaskList.test.js
+++ src/__TEST__/TaskList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TaskList } from './TaskList';
-import { withPinnedTasks } from './TaskList.stories';
+import { TaskList } from '../components/TaskList';
+import { withPinnedTasks } from '../stories/TaskList.stories';
 
 it('renders pinned tasks at the start of the list', () => {
   const div = document.createElement('div');
