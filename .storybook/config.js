16240cd70d9d2f854a898be501291c1d52bc3203
damien <damien.savoldelli@gmail.com>
2019-11-29 14:14:15 +0100

    Install viewport addon storybook & update configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,24 @@
 import { configure } from '@storybook/react';
-import '../src/index.css';
+
+
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
 import requireContext from 'require-context.macro';
 import 'storybook-chromatic';
+
+import '../src/index.css';
 
 const req = requireContext('../src/__STORIES__', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
 
 configure(loadStories, module);
